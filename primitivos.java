4127f8ebdd355388a6d77b0bd9af8c848f0e5008
Marcos Aleixo <maknytz@gmail.com>
2026-03-12 20:29:09 -0300

    Tipos primitivos no java

---
+++ primitivos.java
@@ -0,0 +1,13 @@
+public class primitivos {
+    public static void main(String[] args) {
+        byte number = 1;
+        System.out.println(number);
+        short num1 = 10;
+        int num2 = 25;
+        long num3 = 5L;
+        float num4 = 3.5F;
+        double num5 = 4.5;
+        char caractere = 'v';
+        boolean bool = false;
+    }
+}
