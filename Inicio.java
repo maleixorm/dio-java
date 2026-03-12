7b2ac51838dadee29dc0c07c646f06184fbc4482
Marcos Aleixo <maknytz@gmail.com>
2026-03-12 18:55:26 -0300

    Criando uma constante no projeto

--- Inicio.java
+++ Inicio.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
 public class Inicio {
+
+    private final static String WELCOME_MESSAGE = "Seja bem-vindo! Informe o seu nome: ";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Olá! Informe o seu nome: ");
+        System.out.println(WELCOME_MESSAGE);
         String name = scanner.next();
         System.out.println("Informe sua idade: ");
         int age = scanner.nextInt();
-        System.out.printf("Ola %s! Sua idade é %s anos. \n", name, age);
+        System.out.printf("Olá %s! Sua idade é %s anos. \n", name, age);
     }
 }
