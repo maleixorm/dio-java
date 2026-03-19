db99903ee603befc4fa3de4c369c26f511876df1
Marcos Aleixo <maknytz@gmail.com>
2026-03-18 21:23:36 -0300

    Funções nativas no java para operadores aritiméticos.

--- OperadoresAritimeticos.java
+++ OperadoresAritimeticos.java
@@ -12,5 +12,7 @@ public class OperadoresAritimeticos {
         System.out.printf("%s * %s = %s\n", value1, value2, value1 * value2);
         System.out.printf("%s / %s = %s\n", value1, value2, value1 / value2);
         System.out.printf("%s %% %s = %s\n", value1, value2, value1 % value2);
+        System.out.printf("A raíz quadrada de %s é: %s\n", value1,  Math.sqrt(value1));
+        System.out.printf("A raíz quadrada de %s é: %s\n", value2,  Math.pow(value2, 3));
     }
 }
