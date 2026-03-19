8792e38f95106e6a1f5e32e9d219f80e0d6d69db
Marcos Aleixo <maknytz@gmail.com>
2026-03-18 22:51:46 -0300

    Bitwise operators

--- OperadoresBitWise.java
+++ OperadoresBitWise.java
@@ -1,15 +1,22 @@
 public class OperadoresBitWise {
     public static void main(String[] args) {
-        var value1 = 6;
+        var value1 = 5;
         var binary1 = Integer.toBinaryString(value1);
         System.out.printf("Primeiro número da operação %s (representação binária %s)\n", value1, binary1);
-        var value2 = 5;
+        var value2 = 6;
         var binary2 = Integer.toBinaryString(value2);
         System.out.printf("Segundo número da operação %s (representação binária %s)\n", value2, binary2);
-        System.out.printf(" %s | %s = %s", value1, value2, value1 | value2);
-        System.out.println("\n");
-        var value3 = 8;
+        var value3 = value1 | value2;
         var binary3 = Integer.toBinaryString(value3);
-        System.out.printf("Terceiro número da operação %s (representação binária %s)\n", value3, binary3);
+        System.out.printf(" %s | %s = %s (representação binária %s)\n", value1, value2, value3, binary3);
+        var value4 = value1 ^ value2;
+        var binary4 = Integer.toBinaryString(value4);
+        System.out.printf(" %s | %s = %s (representação binária %s)\n", value1, value2, value4, binary4);
+        var value5 = value1 << value2;
+        var binary5 = Integer.toBinaryString(value5);
+        System.out.printf(" %s | %s = %s (representação binária %s)\n", value1, value2, value5, binary5);
+        var value6 = value1 >> value2;
+        var binary6 = Integer.toBinaryString(value6);
+        System.out.printf(" %s | %s = %s (representação binária %s)\n", value1, value2, value6, binary6);
     }
 }
